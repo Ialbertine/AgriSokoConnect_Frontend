94328c5b5c39c06be4b0c1b0aa85364eee09c6dc
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-05-10 11:50:05 +0200

    About us page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -22,40 +22,40 @@ const About = () => {
                             <p className='text-4xl text-green-900'>Harvest Showcasing</p>
                             <p className='text-lg text-justify'>Cooperatives celebrating the food they grow, proudly show them off to everyone
                                 and throughout the day, they engage with the distributors and vendors for increasing MARKET ACCESS
-                                bridge the gap between farmers and buyers, promote agricultural development, and empower farmers through technology. 
+                                bridge the gap between farmers and buyers, promote agricultural development, and empower farmers through technology.
                             </p>
-                            <p className=''><i>It is marvelous seeing farmers benefiting from what they have made </i></p><PiHandsClappingThin/>
+                            <p className=''><i>It is marvelous seeing farmers benefiting from what they have made </i></p><PiHandsClappingThin />
                         </div>
                         <div>
                             <img src='Growth.jpg' className='lg:w-[66vh] md:w-[56vh] sm:w-[66vh] rounded-lg'></img>
                         </div>
                     </div>
                 </div>
-                <div className='flex lg:flex-row md:flex-col sm:flex-col items-center justify-center gap-20 text-white px-40 py-20 bg-cover bg-no-repeat'
-                    style={{backgroundImage: "url('Mission.jpg')"}}
+                <div className='flex items-center justify-center gap-20 text-white px-40 py-20 bg-cover bg-no-repeat'
+                    style={{ backgroundImage: "url('Mission.jpg')" }}
                 >
                     <div className='flex flex-col gap-7'>
                         <p className='text-2xl'>MISSION</p>
                         <div>
-                            <p className='border-r-2 border-gray-400 lg:w-[50vh] md:w-[70vh] sm:w-[40vh] text-lg pr-5'>create a transparent, efficient, and inclusive agricultural marketplace that connects farmers, buyers, 
-                                and other stakeholders, enabling fair and profitable transactions while promoting sustainability 
-                                and economic growth in rural communities. 
+                            <p className='border-r-2 border-gray-400 w-[50vh] text-lg pr-5'>create a transparent, efficient, and inclusive agricultural marketplace that connects farmers, buyers,
+                                and other stakeholders, enabling fair and profitable transactions while promoting sustainability
+                                and economic growth in rural communities.
                             </p>
                         </div>
 
                     </div>
                     <div className='flex flex-col gap-7'>
                         <p className='text-2xl'>VISION</p>
-                        <p className='border-r-2 border-gray-400 lg:w-[50vh] md:w-[70vh] sm:w-[40vh] text-lg pr-5'>                                                    
-                            revolutionize agricultural trade by leveraging technology to bridge the gap 
-                            between producers and consumers, ensuring food security, empowering farmers, and fostering prosperity 
+                        <p className='border-r-2 border-gray-400 w-[50vh] text-lg pr-5'>
+                            revolutionize agricultural trade by leveraging technology to bridge the gap
+                            between producers and consumers, ensuring food security, empowering farmers, and fostering prosperity
                             across the agricultural value chain.
                         </p>
                     </div>
                     <div className='flex flex-col gap-7'>
                         <p className='text-2xl'>VALUES</p>
                         <div>
-                            <ol className='lg:w-[50vh] md:w-[70vh] sm:w-[40vh] text-lg'>
+                            <ol className=' text-lg'>
                                 <li>Transparency</li>
                                 <li>Empowerment</li>
                                 <li>Sustainability</li>
