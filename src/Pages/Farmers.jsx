d7d530e21f4b19b19938d4f20cb3b5f6ad68ced6
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-05-22 09:56:29 +0200

    fetching farmers

--- src/Pages/Farmers.jsx
+++ src/Pages/Farmers.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Dashboard/Navbar'
+import axios from 'axios';
 
 const Farmers = () => {
+
+  const [farmers, setFarmers] = useState([]);
+  const [token, setToken] =useState();
+
+  const getToken = () =>{
+    let Token = localStorage.getItem(token);
+    setToken(Token);
+  }
+
+  useEffect(() =>{
+    getToken();
+  }, [])
+
+  const FetchFarmers = () =>{
+    axios({
+      method:'GET',
+      url:'',
+      headers:{
+        Accept:'application/json',
+        Authorization:`Bearer ${token}`
+      }
+    })
+    .then((response) =>{
+      console.log(response);
+      setFarmers(response);
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+  }
+
+  useEffect(() =>{
+    FetchFarmers();
+  })
+
   return (
     <>
-    {/* <Navbar/> */}
+    
     </>
   )
 }
